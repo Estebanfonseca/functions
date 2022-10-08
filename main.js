8b9f0087eebfac747082be7c772bc0a47a62cf3c
Estebanfonseca <estebanp57@hotmail.com>
2022-10-08 13:27:44 -0500

    1 ejercicio

--- main.js
+++ main.js
@@ -2,12 +2,31 @@ console.log(beer)
 function cervezas (array, grades){
     let beeer = []
     for(let beer of array){
-        if(parseInt(beer.abv) <= grades ){
+        if(beer.abv <= parseFloat(grades) ){
             beeer.push(beer)
         }
-    }
+    } 
     return beeer
+    
 }
 
-let cerveza = cervezas(beer, 4)
-console.log(cerveza)
+let cerveza = cervezas(beer,5)
+console.log( cerveza.map( elem => ({name:elem.name, abv:elem.abv ,ibu : elem.ibu})))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

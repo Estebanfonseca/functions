3cfc1d79ab7a04a51fb0d3ee22faaf4049174c9b
Estebanfonseca <estebanp57@hotmail.com>
2022-10-08 16:05:25 -0500

    2 funciones mas

--- main.js
+++ main.js
@@ -6,12 +6,50 @@ function cervezas (array, grades){
             beeer.push(beer)
         }
     } 
-    return beeer
-    
+    return beeer.map( elem => ({name:elem.name, abv:elem.abv ,ibu : elem.ibu}))
 }
 
 let cerveza = cervezas(beer,5)
-console.log( cerveza.map( elem => ({name:elem.name, abv:elem.abv ,ibu : elem.ibu})))
+console.log(cerveza)
+
+function las10Mas(array){
+    array.sort((a,b)=>{
+        if(a.abv > b.abv){
+            return -1
+        }else if(a.abv < b.abv){
+            return 1
+        }else {
+            return 0
+        }
+    })
+    return array.slice(0,10)
+}
+
+let losMas = las10Mas(beer)
+console.log(losMas)
+
+function las10Menos (array){
+    array.sort((a,b)=>{
+        if(a.ibu < b.ibu){
+            return -1
+        }else if(a.ibu > b.ibu){
+            return 1
+        }else {
+            return 0
+        }
+    })
+    return array.slice(0,10)
+}
+
+let losMenos = las10Menos(beer)
+console.log(losMenos)
+
+
+
+
+
+
+
 
 
 
